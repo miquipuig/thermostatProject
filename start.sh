cd26482488cceb8ff858b9a42f36e24e5571d46f
Miquel Puig <mpuig22@gmail.com>
2020-02-11 01:55:40 +0100

    auto commit2

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 export DISPLAY=:0
+xinit
 xset q
 xset -dpms
 xset s noblank
 xset s off 
-xinit /bin/sh -c 'exec python3 thermostatProject/thermostatStart.py' &
+# xinit /bin/sh -c 'exec python3 thermostatProject/thermostatStart.py' &
+python3 thermostatProject/thermostatStart.py &
--- thermostatSave.py
+++ thermostatSave.py
@@ -3,12 +3,11 @@ from thermostatGPIO import tg
 
 
 class ThermostatSave(object):
-    def __init__(self, interval=1):
-        self.interval = interval          
+    def __init__(self):        
         thread = threading.Thread(target=self.run, args=())
         thread.daemon = True
         thread.start()
 
 
     def run(self):
-        tg.saveloop(1)
+        tg.saveloop(3)
